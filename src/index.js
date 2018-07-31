9f8499c2ee8a1bdf2085661bb8d152bf187e5b0e
puan0601 <anton.pugach@gmail.com>
2018-07-30 23:14:00 -0700

    wires props into squares

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+class Square extends Component {
     render() {
         return (
             <button className="square">
-            { /* TODO */ }
+            {this.props.value}
             </button>
         );
     }
 }
 
-class Board extends React.Component {
+class Board extends Component {
     renderSquare(i) {
-        return <Square />;
+        return <Square value={i} />;
     }
 
     render() {
@@ -43,7 +43,7 @@ class Board extends React.Component {
     }
 }
 
-class Game extends React.Component {
+class Game extends Component {
     render() {
         return (
             <div className="game">
